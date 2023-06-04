a0f2c36fc904ed5528ea25e6f1cfbb89bdad2a8d
Eike Tessars <tessars@physik.uni-kiel.de>
2023-06-04 16:56:37 +0200

    wip

--- Optisches_Pumpen/B_sweep.py
+++ Optisches_Pumpen/B_sweep.py
@@ -2,6 +2,7 @@ import numpy as np
 import matplotlib.pyplot as plt
 import linecache
 from scipy.optimize import curve_fit
+from scipy.signal import find_peaks
 
 class Oszi:
     def __init__(self,path):
@@ -12,6 +13,16 @@ class Oszi:
         self.time = data[0]
         self.ch1 = data[1]
         self.ch2 = data[2]
+        # print(np.arange(0.001,0.01,0.0001))
+        for prom in np.arange(0.001,0.01,0.0001):
+            # print(prom)
+            self.peaks,_=find_peaks(-self.ch2,prominence=prom,distance=10)
+            if len(self.peaks)==7:
+                # print(prom,"Erfolg")
+                # print("yes")
+                break
+        # print(prom)
+        # print(self.peaks)
         
 
     def plot(self,xlabel=None,ylabel1=None,ylabel2=None,title=None,figsize=(8,5),ScopeSettings=False):
@@ -22,13 +33,15 @@ class Oszi:
             '''Bin mir nicht ganz sicher, ob das so passt. Wahrscheinliche automatische Anpassung besser'''
             offset1 = float(linecache.getline(self.scope,3).split()[1])
             offset2 = float(linecache.getline(self.scope,11).split()[1])
-            print(offset1)
-            print(offset2)
+            # print(offset1)
+            # print(offset2)
             ax1.plot(self.time,self.ch1+offset1,label='Channel 1',color='C0')
             ax2.plot(self.time,self.ch2+offset2,label='Channel 2',color="C1")
+            ax2.scatter(self.time[self.peaks],self.ch2[self.peaks]+offset2,label='Channel 2',color='C2')
         else:
             ax1.plot(self.time,self.ch1,label='Channel 1',color='C0')
             ax2.plot(self.time,self.ch2,label='Channel 2',color='C1')
+            ax2.scatter(self.time[self.peaks],self.ch2[self.peaks],label='Channel 2',color='C1')
         ax1.tick_params(axis='y', labelcolor='C0')
         ax2.tick_params(axis='y', labelcolor='C1')
         ax1.legend(loc=2)
@@ -45,6 +58,26 @@ def main():
     a=Oszi("data/g_faktor/B_rampe/500kHz")
     a.plot(ScopeSettings=True,ylabel1="U in V Channel 1",ylabel2="U in V Channel 2",title="Orientierungsezit")
     plt.show()
+    f_list=np.append(750,np.arange(1000,10001,500))
+    peak1=[]
+    peak2=[]
+    peak3=[]
+    peak4=[]
+    for f in f_list:
+        a=Oszi(f"data/g_faktor/B_rampe/{f}kHz")
+        print(len(a.peaks))
+        peak1.append(a.peaks[1])
+        peak2.append(a.peaks[2])
+        peak3.append(a.peaks[4])
+        peak4.append(a.peaks[5])
+    plt.plot(peak1)
+    plt.plot(peak2)
+    plt.plot(peak3)
+    plt.plot(peak4)
+    plt.show()
+
+
+
 
 
 if __name__=="__main__":
