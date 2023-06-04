55f463ee7bc54d2d4653f03a4383c07f4b25ba48
Eike Tessars <tessars@physik.uni-kiel.de>
2023-06-04 16:01:42 +0200

    wip

--- Optisches_Pumpen/dt.py
+++ Optisches_Pumpen/dt.py
@@ -3,6 +3,7 @@ import matplotlib.pyplot as plt
 from matplotlib.gridspec import GridSpec
 import pandas as pd
 import linecache
+from scipy.optimize import curve_fit
 
 # class fourier:
 #     def __init__(self,f: float,max_n: int,t: np.ndarray) -> None:
@@ -36,7 +37,7 @@ import linecache
 #         plt.show()
 
 class Oszi:
-    def __init__(self,path):
+    def __init__(self,path,f):
         '''Path ohne Endung angeben.'''
         data = np.genfromtxt(path).T
         self.path = path
@@ -44,6 +45,35 @@ class Oszi:
         self.time = data[0]
         self.ch1 = data[1]
         self.ch2 = data[2]
+        self.omega=f*2*np.pi
+        self.fourier_U,self.fourier_I = self.get_U_fourier()
+
+    def get_U_fourier(self):
+        U_arr=np.zeros(len(self.time))
+        I_arr=np.zeros(len(self.time))
+        self.L=0.0047
+        self.R=1.7223
+        t_off=0.0002
+        for ti,t in enumerate(self.time):
+            for n in range(1,5000):
+                U_arr[ti]+=np.sin((2*n-1)*self.omega*(t-t_off))/(2*n-1)
+                I_arr[ti]+=np.sin((2*n-1)*self.omega*(t-t_off)-np.arctan((2*n-1)*self.omega*self.L/self.R))/((2*n-1)**2*self.omega**2*self.L**2*+self.R**2)
+        return U_arr,I_arr
+
+    def get_L(self):
+        f_exp = lambda t,offset,a,b: b*np.exp(a*t)+offset 
+        self.time_mod = self.time[55:]
+        self.ch1_mod = self.ch1[55:]
+        self.ch2_mod = self.ch2[55:]
+        self.popt, self.pcov = curve_fit(f_exp,self.time_mod,self.ch1_mod)
+        self.L=1.7223/self.popt[1]*-1
+        self.L_err=np.abs(np.sqrt(self.pcov[1][1])*1.7223/self.popt[1]**2)+np.abs(0.0017/self.popt[1]*-1)
+        print(f"L ist {self.L} pm {self.L_err}")
+
+    def exp_fit(self,t):
+        f_exp = lambda t,offset,a,b: b*np.exp(a*t)+offset 
+        return f_exp(t,self.popt[0],self.popt[1],self.popt[2])
+        
 
     def plot(self,xlabel=None,ylabel1=None,ylabel2=None,title=None,figsize=(8,5),ScopeSettings=False):
         '''ScopeSettings gibt an, ob Oszi-Fenster wie im Praktikum dargestellt werden soll.'''
@@ -57,31 +87,40 @@ class Oszi:
             # scale2 = float(linecache.getline(self.scope,10).split()[1])
             print(offset1)
             print(offset2)
-            ax1.plot(self.time,self.ch1+offset1,label='Channel 1',color='blue')
-            ax2.plot(self.time,self.ch2+offset2,label='Channel 2',color='red')
+            ax1.plot(self.time,self.ch1+offset1,label='Channel 1',color='C0')
+            # ax1.plot(self.time_mod,self.exp_fit(self.time_mod),label='Channel 1',color='C0')
+            ax2.plot(self.time,self.ch2+offset2,label='Channel 2',color="C1")
+            ax1.plot(self.time,self.fourier_U,label='Fourier Spannung',color='C0')
+            ax2.plot(self.time,self.fourier_I,label='Fourier Strom',color='C1')
             ax1.legend(loc=2)
             ax2.legend(loc=3)
             ax1.tick_params(axis='y', labelcolor='blue')
             ax2.tick_params(axis='y', labelcolor='red')
         else:
-            ax1.plot(self.time,self.ch1,label='Channel 1',color='blue')
-            ax2.plot(self.time,self.ch2,label='Channel 2',color='red')
-            ax1.legend(loc=2)
-            ax2.legend(loc=3)
-            ax1.tick_params(axis='y', labelcolor='blue')
-            ax2.tick_params(axis='y', labelcolor='red')
+            ax1.plot(self.time,self.ch1,label='Channel 1',color='C0')
+            ax2.plot(self.time,self.ch2,label='Channel 2',color='C1')
+            ax1.plot(self.time,self.fourier_U,label='Fourier Spannung',color='C0')
+            ax2.plot(self.time,self.fourier_I,label='Fourier Strom',color='C1')
+            # ax1.plot(self.time_mod,self.exp_fit(self.time_mod),label='Channel 1 fit',color='C1',ls="--")
+            # ax1.legend(loc=3)
+            # ax2.legend(loc=3)
+            ax1.tick_params(axis='y', labelcolor='C0')
+            ax2.tick_params(axis='y', labelcolor='C1')
         ax1.set_xlabel(xlabel)
-        ax1.set_ylabel(ylabel1,c='blue')
-        ax2.set_ylabel(ylabel2,c='red')
+        ax1.set_ylabel(ylabel1,c="C0")
+        ax2.set_ylabel(ylabel2,c='C1')
         plt.title(title)
+        plt.tight_layout()
+        plt.savefig("figures/dt/dt_lang.pdf")
         return fig, ax1, ax2
 
-def main():
-    a=Oszi("data/ohne_B/l_messung/30Hz_5Vpp")
-    a.plot(ScopeSettings=False)
+def dt():
+    a=Oszi("data/ohne_B/l_messung/30Hz_5Vpp",30)
+    a.get_L(1901)
+    a.plot(ScopeSettings=True,ylabel1="U in V",ylabel2="U in V",title="Messung der Induktivität")
     plt.show()
     # b=fourier(0.25,1000,np.arange(0,10,0.01))
 
 
 if __name__=="__main__":
-    main()
+    dt()
--- Optisches_Pumpen/orientierungszeit.py
+++ Optisches_Pumpen/orientierungszeit.py
@@ -12,6 +12,22 @@ class Oszi:
         self.time = data[0]
         self.ch1 = data[1]
         self.ch2 = data[2]
+        self.fit=False
+
+    def get_fit(self,start,stop,p0):
+        f_exp = lambda t,offset,a,b: b*np.exp(a*t)+offset 
+        self.time_mod = self.time[start:stop]
+        self.ch1_mod = self.ch1[start:stop]
+        self.ch2_mod = self.ch2[start:stop]
+        self.popt, self.pcov = curve_fit(f_exp,self.time_mod,self.ch2_mod,maxfev=10000,p0=p0)
+        self.tau=-1/self.popt[1]
+        self.tau_err=np.abs(np.sqrt(self.pcov[1][1])/self.popt[1]**2)
+        print(f"tau ist {self.tau} pm {self.tau_err}")
+        self.fit=True
+
+    def exp_fit(self,t):
+        f_exp = lambda t,offset,a,b: b*np.exp(a*t)+offset 
+        return f_exp(t,self.popt[0],self.popt[1],self.popt[2])
         
 
     def plot(self,xlabel=None,ylabel1=None,ylabel2=None,title=None,figsize=(8,5),ScopeSettings=False):
@@ -26,9 +42,13 @@ class Oszi:
             print(offset2)
             ax1.plot(self.time,self.ch1+offset1,label='Channel 1',color='C0')
             ax2.plot(self.time,self.ch2+offset2,label='Channel 2',color="C1")
+            if self.fit:
+                ax2.plot(self.time_mod,self.exp_fit(self.time_mod)+offset2,label='Channel 2 fit',color='C1',ls="--")
         else:
             ax1.plot(self.time,self.ch1,label='Channel 1',color='C0')
             ax2.plot(self.time,self.ch2,label='Channel 2',color='C1')
+            if self.fit:
+                ax2.plot(self.time_mod,self.exp_fit(self.time_mod),label='Channel 2 fit',color='C1',ls="---")
         ax1.tick_params(axis='y', labelcolor='C0')
         ax2.tick_params(axis='y', labelcolor='C1')
         ax1.legend(loc=2)
@@ -38,11 +58,15 @@ class Oszi:
         ax2.set_ylabel(ylabel2,c='C1')
         plt.title(title)
         plt.tight_layout()
-        plt.savefig("figures/dt/test.pdf")
+        plt.savefig("figures/dt/tau_bsp.pdf")
         return fig, ax1, ax2
 
 def main():
-    a=Oszi("data/g_faktor/B_rampe/500kHz")
+    a=Oszi("data/ohne_B/orientierungszeit/30Hz_5Vpp")
+    start_stop_30=[420,1901]
+    start_stop_60=[200,2000]
+    start_stop_100=[700,1700]
+    a.get_fit(start_stop_30[0],start_stop_30[1],[1.6,-1/0.00295,-0.1])
     a.plot(ScopeSettings=True,ylabel1="U in V Channel 1",ylabel2="U in V Channel 2",title="Orientierungsezit")
     plt.show()
 
